183b320fc6323446c3dee645a03521eb96c4d51f
rvermaas-12 <80804058+rvermaas-12@users.noreply.github.com>
2021-05-24 16:05:14 +1000

    third commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,9 +1,5 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
 
--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -1,43 +1,57 @@
 package com.h2;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.Map;
 
 public class SavingsCalculator {
-    private static float[] debits;
-    private static float[] credits;
+    private float[] credits;
+    private float[] debits;
 
-
-    public SavingsCalculator(float credit, float debit) {
-        this.credits = credit;
-        this.debits = debit;
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
     }
 
-    private static float sumOfCredits() {
+    private float sumOfCredits() {
         float sum = 0.0f;
-        for (float theirCredits : credits) {
-            sum += theirCredits;
+        for(float credit: credits) {
+            sum += credit;
         }
         return sum;
     }
 
-    private static float sumOfDebits() {
+    private float sumOfDebits() {
         float sum = 0.0f;
-        for (float theirDebits : debits) {
-            sum += theirDebits;
+        for(float debit: debits) {
+            sum += debit;
         }
         return sum;
     }
 
-    private static int remainingDaysInMonth(localDate) {
-        Object date = localDate;
-        YearMonth = yearMonth;
-        return -1;
+    private static int remainingDaysInMonth(LocalDate date) {
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+        return remainingDays;
     }
 
-    public static final Map<Integer, Float> bestRates = Map.of(
-            1, 5.50f,
-            2, 3.45f,
-            3, 2.67f
-    );
+    public float calculate() {
+        return sumOfDebits() - sumOfCredits();
+    }
+
+    public static void main(String[] args) {
+        final String[] creditsAsString = args[0].split(",");
+        final String[] debitAsAsString = args[1].split(",");
+        float[] credits = new float[creditsAsString.length];
+        float[] debits = new float[debitAsAsString.length];
+
+        for(int i = 0; i < creditsAsString.length; i++) {
+            credits[i] = Float.parseFloat(creditsAsString[i]);
+        };
+        for(int i = 0; i < debitAsAsString.length; i++) {
+            debits[i] = Float.parseFloat(debitAsAsString[i]);
+        };
+        final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+        float netSavings = calculator.calculate();
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
+    }
 }
