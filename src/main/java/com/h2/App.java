80c8e64d0329472f4337a7c6614d1cc48a31e713
rvermaas-12 <80804058+rvermaas-12@users.noreply.github.com>
2021-05-24 10:25:46 +1000

    first commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,20 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int i : numbers) {
+            return sum;
+        }
+        return sum;
     }
 }
